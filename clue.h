87d29f28964e447460a91f1a1895c8ac379f1324
tanvigprasad <tanvigprasad@gmail.com>
2026-04-05 12:05:57 -0500

    Create clue.h

---
+++ clue.h
@@ -0,0 +1,51 @@
+#ifndef CLUE_H
+#define CLUE_H
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+class clue {
+  private:
+    char symbol;
+    string question, correctAnswer;
+    int points, maxAttempts;
+    bool isCompleted = false;
+
+  public:
+//constructor
+    clue(char s, string q, string ans, int attempts, int pts) {
+        symbol = s;
+        question = q;
+        correctAnswer = ans;
+        maxAttempts = attempts;
+        points = pts;
+        isCompleted = false;
+    }
+
+//getter methods
+    char getSymbol() {
+        return symbol;
+    }
+
+    string getQuestion(){
+        return question;
+    }
+
+    string getCorrectAnswer(){
+        return correctAnswer;
+    }
+
+    int getPoints(){
+        return points;
+    }
+
+    bool isSolved(){
+        return isCompleted;
+    }
+
+//logic functions
+    bool checkAnswer(string& playerGuess);
+    static string toLowerCopy(const string& s)
+
+};
