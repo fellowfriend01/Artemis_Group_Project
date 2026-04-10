def739321512953ad4c761fa5b82b06f7ae79913
noahjohnson88 <noahj0661@gmail.com>
2026-04-10 00:04:27 -0500

    Implement main function for Treasure Hunt game

--- finalmain.cpp
+++ finalmain.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "TreasureHunt.h"
+using namespace std;
 
+int main(){
+    cout << "Welcome to the UT Campus Treasure Hunt!" << endl;
+
+    TreasureHunt game;
+    game.loadMap("map.txt");
+    game.loadClues("clues.txt");
+    game.playGame();
+
+    return 0;
+}
